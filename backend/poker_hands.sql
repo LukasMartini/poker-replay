d1a2c1382deedde5769f38b4ce9cf4f9209d5ebd
Matteo_psnt <matteo@szl.ai>
2024-05-30 18:15:06 -0400

    refactor and change to snake_case

--- backend/poker_hands.sql
+++ backend/poker_hands.sql
@@ -1,50 +1,58 @@
-CREATE TABLE PokerHands (
-    HandID SERIAL PRIMARY KEY,
-    PokerStarsHandID BIGINT,
-    GameType VARCHAR(50),
-    Stakes VARCHAR(20),
-    DateTime TIMESTAMP,
-    TableName VARCHAR(50),
-    SeatNumber INT
+CREATE TABLE poker_hand (
+    id SERIAL PRIMARY KEY,
+    hand_id BIGINT,
+    game_type VARCHAR(50),
+    stakes VARCHAR(20),
+    played_at TIMESTAMP,
+    table_name VARCHAR(50),
+    seat_number INT
 );
 
-CREATE TABLE Players (
-    PlayerID SERIAL PRIMARY KEY,
-    PlayerName VARCHAR(50) NOT NULL
+CREATE TABLE player (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE PlayerActions (
-    ActionID SERIAL PRIMARY KEY,
-    PlayerID INT,
-    HandID INT,
-    SeatNumber INT,
-    ActionType VARCHAR(50),
-    Amount NUMERIC(10, 2),
-    Card1 CHAR(2),
-    Card2 CHAR(2),
-    FOREIGN KEY (PlayerID) REFERENCES Players(PlayerID),
-    FOREIGN KEY (HandID) REFERENCES PokerHands(HandID)
+CREATE TABLE player_action (
+    id SERIAL PRIMARY KEY,
+    player_id INT,
+    hand_id INT,
+    seat_number INT,
+    action_type VARCHAR(50),
+    amount NUMERIC(10, 2),
+    stack_size NUMERIC(10, 2),
+    FOREIGN KEY (player_id) REFERENCES player(id),
+    FOREIGN KEY (hand_id) REFERENCES poker_hand(id)
 );
 
-CREATE TABLE BoardCards (
-    BoardID SERIAL PRIMARY KEY,
-    HandID INT,
-    FlopCard1 CHAR(2) NULL,
-    FlopCard2 CHAR(2) NULL,
-    FlopCard3 CHAR(2) NULL,
-    TurnCard CHAR(2) NULL,
-    RiverCard CHAR(2) NULL,
-    FOREIGN KEY (HandID) REFERENCES PokerHands(HandID)
+CREATE TABLE board_card (
+    id SERIAL PRIMARY KEY,
+    hand_id INT,
+    flop_card1 CHAR(2) NULL,
+    flop_card2 CHAR(2) NULL,
+    flop_card3 CHAR(2) NULL,
+    turn_card CHAR(2) NULL,
+    river_card CHAR(2) NULL,
+    FOREIGN KEY (hand_id) REFERENCES poker_hand(id)
 );
 
-CREATE TABLE HandSummary (
-    SummaryID SERIAL PRIMARY KEY,
-    HandID INT,
-    TotalPot NUMERIC(10, 2),
-    Rake NUMERIC(10, 2),
-    Board VARCHAR(20),
-    WinnerID INT,
-    WinningHand VARCHAR(50),
-    FOREIGN KEY (HandID) REFERENCES PokerHands(HandID),
-    FOREIGN KEY (WinnerID) REFERENCES Players(PlayerID)
+CREATE TABLE hand_summary (
+    id SERIAL PRIMARY KEY,
+    hand_id INT,
+    total_pot NUMERIC(10, 2),
+    rake NUMERIC(10, 2),
+    board VARCHAR(20),
+    FOREIGN KEY (hand_id) REFERENCES poker_hand(id)
+);
+
+CREATE TABLE player_card (
+    id SERIAL PRIMARY KEY,
+    hand_id INT,
+    player_id INT,
+    hole_card1 CHAR(2),
+    hole_card2 CHAR(2),
+    position INT,
+    stack_size NUMERIC(10, 2),
+    FOREIGN KEY (hand_id) REFERENCES poker_hand(id),
+    FOREIGN KEY (player_id) REFERENCES player(id)
 );
