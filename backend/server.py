41838d27f99b2f14bad15439f67f7fb757d14390
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2024-06-13 16:28:00 -0400

    Removed boilerplate in server.py.

--- backend/server.py
+++ backend/server.py
@@ -3,29 +3,31 @@ from db_commands import get_db_connection
 
 app = Flask(__name__)
 
+conn = get_db_connection()
+
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM books;')
-    books = cur.fetchall()
-    cur.close()
-    conn.close()
-    return Response(response=str(books), status=200)
+    return Response(response="Hello World!", status=200)
+    # cur = conn.cursor()
+    # cur.execute('SELECT * FROM books;')
+    # books = cur.fetchall()
+    # cur.close()
+    # conn.close()
+    # return Response(response=str(books), status=200)
 
 @app.route('/fetch_hand', methods=['GET'])
-def fetch_hand(hand_id: int, username: str, conn: psycopg2.connect) -> str:
+def fetch_hand(hand_id: int, username: str) -> str:
     cursor = conn.cursor()
     with open ('backend/sql/fetch_hand_query_template.sql') as fetch_hand_query_template:
         cursor.execute(fetch_hand_query_template.read())
-        cursor.execute(f'EXECUTE hand_data({hand_id}, \'{username}\')') # TODO: remove security vulnerability.
+        cursor.execute(f'EXECUTE hand_data({hand_id}, \'{username}\')') 
         output: str = cursor.fetchall()
 
     cursor.close()
 
-    return output
+    return Response(response=output, status=200)
 
-print(fetch_hand(1, 'Ted', get_db_connection()))
+print(fetch_hand(1, 'Ted'))
 
 
 if __name__ == '__main__':
