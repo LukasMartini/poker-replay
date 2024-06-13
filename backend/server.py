daf5249d050b965676c02a87b7e40426659ee71c
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2024-06-13 18:07:47 -0400

    Refactored server.py to have separate api calls for each query template. gitignore changed as required.

--- backend/server.py
+++ backend/server.py
@@ -1,32 +1,48 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 from db_commands import get_db_connection
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
 conn = get_db_connection()
 
-# @app.route('/api/hands', methods=['GET'])
-
-
-@app.route('/api/hands/<int:id>', methods=['GET'])
+@app.route('/api/hand_summary/<int:id>', methods=['GET'])
 @cross_origin()
-def hands(id: int, username: str='test') -> str:
+def hand_summary(id: int) -> Response:
     cursor = conn.cursor()
-    # with open ('./sql/fetch_hand_query_template.sql') as fetch_hand_query_template:
-    #     cursor.execute(fetch_hand_query_template.read())
-    #     cursor.execute(f'EXECUTE hand_data2({id}, \'{username}\')') 
-    #     output: str = cursor.fetchall()
-
+    cursor.execute(f'EXECUTE one_time_hand_info({id})')
+    result = cursor.fetchall()
     cursor.close()
 
-    response = Response(response='''{"test": 320320392}''', status=200)
-    return response
+    return Response(response=jsonify(result), status=200)
 
-# print(fetch_hand(1, 'Ted'))
+@app.route("/api/player_actions/<int:id>")
+@cross_origin
+def player_actions(id: int) -> Response:
+    cursor = conn.cursor()
+    cursor.execute(f'EXECUTE player_actions_in_hand({id})')
+    result = cursor.fetchall()
+    cursor.close()
 
+    return Response(response=jsonify(result), status=200)
+
+@app.route("/api/player_cards/<int:id>")
+@cross_origin
+def player_cards(id: int) -> Response:
+    cursor = conn.cursor()
+    cursor.execute(f'EXECUTE player_cards_in_hand({id})')
+    result = cursor.fetchall()
+    cursor.close()
+
+    return Response(response=jsonify(result), status=200)
 
 if __name__ == '__main__':
+    app = Flask(__name__)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
+    cursor = conn.cursor()
+    cursor.execute(open('./sql/fetch_hand_query_templates.sql').read())
+
     app.run(host="localhost", port=5001, debug=True)
+
+    cursor.close()
+    conn.close()
