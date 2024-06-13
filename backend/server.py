a8013b7e31ab51554f537d94f2ef5f422c6e304b
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2024-06-13 18:41:25 -0400

    Refactored to use execute_query from db_commands.py.

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,8 @@
 from flask import Flask, Response, jsonify
-from db_commands import get_db_connection
+from db_commands import get_db_connection, execute_query
 from flask_cors import CORS, cross_origin
 
-conn = get_db_connection()
+# conn = get_db_connection()
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,39 +11,26 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/api/hand_summary/<int:id>', methods=['GET'])
 @cross_origin()
 def hand_summary(id: int) -> Response:
-    cursor = conn.cursor()
-    cursor.execute(f'EXECUTE one_time_hand_info({id})')
-    result = cursor.fetchall()
-    cursor.close()
+    result = execute_query(f'EXECUTE one_time_hand_info({id})')
 
     return Response(response=jsonify(result), status=200)
 
 @app.route("/api/player_actions/<int:id>")
 @cross_origin()
 def player_actions(id: int) -> Response:
-    cursor = conn.cursor()
-    cursor.execute(f'EXECUTE player_actions_in_hand({id})')
-    result = cursor.fetchall()
-    cursor.close()
+    result = execute_query(f'EXECUTE player_actions_in_hand({id})')
 
     return Response(response=jsonify(result), status=200)
 
 @app.route("/api/player_cards/<int:id>")
 @cross_origin()
 def player_cards(id: int) -> Response:
-    cursor = conn.cursor()
-    cursor.execute(f'EXECUTE player_cards_in_hand({id})')
-    result = cursor.fetchall()
-    cursor.close()
+    result = execute_query(f'EXECUTE player_cards_in_hand({id})')
 
     return Response(response=jsonify(result), status=200)
 
 if __name__ == '__main__':
-    cursor = conn.cursor()
-    cursor.execute(open('backend/sql/fetch_hand_query_templates.sql').read())
-    cursor.execute(f'EXECUTE one_time_hand_info({2})')
+    execute_query(open('backend/sql/fetch_hand_query_templates.sql').read())
+    execute_query(f'EXECUTE one_time_hand_info({2})')
 
     app.run(host="localhost", port=5001, debug=True)
-
-    cursor.close()
-    conn.close()
