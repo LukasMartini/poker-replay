f7e076ead0b03dced04c924d9dd005d7d30b03ac
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2024-06-13 16:12:57 -0400

    Moved important files from lm-local.

--- backend/server.py
+++ backend/server.py
@@ -13,6 +13,20 @@ def index():
     conn.close()
     return Response(response=str(books), status=200)
 
+@app.route('/fetch_hand', methods=['GET'])
+def fetch_hand(hand_id: int, username: str, conn: psycopg2.connect) -> str:
+    cursor = conn.cursor()
+    with open ('backend/sql/fetch_hand_query_template.sql') as fetch_hand_query_template:
+        cursor.execute(fetch_hand_query_template.read())
+        cursor.execute(f'EXECUTE hand_data({hand_id}, \'{username}\')') # TODO: remove security vulnerability.
+        output: str = cursor.fetchall()
+
+    cursor.close()
+
+    return output
+
+print(fetch_hand(1, 'Ted', get_db_connection()))
+
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5001, debug=True)
