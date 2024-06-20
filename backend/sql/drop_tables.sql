d11292a38f1d76e658f2fc2b9edcb408bc9e69d3
Matteo_psnt <matteo@szl.ai>
2024-06-20 18:09:36 -0400

    Changed order to fix drop tables

--- backend/sql/drop_tables.sql
+++ backend/sql/drop_tables.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS board_cards;
 DROP TABLE IF EXISTS player_cards;
 DROP TABLE IF EXISTS player_action;
 DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS authorized;
 DROP TABLE IF EXISTS poker_hand;
 DROP TABLE IF EXISTS poker_session;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS authorized;
 DROP FUNCTION IF EXISTS is_valid_card;
 
 SELECT 'All specified tables have been dropped successfully.' AS status;
