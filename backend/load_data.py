ac759bfaeec284f4c2bedd3386379f751c0305d8
Eric Zhang <erizha.grad2022@my.stmc.bc.ca>
2024-06-20 17:01:59 -0400

    Update load_data.py

--- backend/load_data.py
+++ backend/load_data.py
@@ -28,5 +28,5 @@ def create_user(username: str, email: str, password: str, token: str):
 if __name__ == "__main__":
     create_user("test_user", "test_email", "test_password", "82fd7671-cfe6-49a6-b3b8-a0df504bf55f")
     print("Parsing hand history...")
-    parse_hand_history("../hand_histories/poker_stars/handHistory-126997.txt", 1)
+    parse_hand_history("../hand_histories/poker_stars/sample_handHistory.txt", 1)
     create_user("user2", "testemail2", "test_password", "f273d736-807e-4f8e-b919-0bc7a558d59b")
