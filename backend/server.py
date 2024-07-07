6252119712b99d7fb784fc4689a506523d967eeb
Matteo Pesenti <40373353+matteo-psnt@users.noreply.github.com>
2024-07-07 17:28:58 -0400

    got upload backend working (#32)
    * got upload backend working
    
    * delete files after

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,11 @@
-from convert_history import parse_hand_history
-from load_data import create_upload, delete_upload, update_upload_status
+import os
 from flask import Flask, Response, jsonify, request
-from db_commands import get_db_connection
 from flask_cors import CORS, cross_origin
-from db_commands import get_hand_count, get_cash_flow
+
+from load_data import create_upload, delete_upload, update_upload_status
+from db_commands import get_db_connection, get_hand_count, get_cash_flow
+from convert_history import parse_hand_history
+
 
 conn = get_db_connection()
 cur = conn.cursor()
@@ -11,13 +13,13 @@ cur = conn.cursor()
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['UPLOAD_FOLDER'] = './uploads/'
 
 
 @app.route('/')
 @cross_origin()
 def homepage():
     return Response(status=200)
-
 
 @app.route('/api/hand_summary/<int:id>', methods=['GET'])
 @cross_origin()
@@ -29,7 +31,6 @@ def hand_summary(id: int) -> Response:
 
     return jsonify(data), 200
 
-
 @app.route("/api/player_actions/<int:id>", methods=['GET'])
 @cross_origin()
 def player_actions(id: int) -> Response:
@@ -39,7 +40,6 @@ def player_actions(id: int) -> Response:
     data = [dict(zip(column_names, row)) for row in result]
 
     return jsonify(data), 200
-
 
 @app.route("/api/player_cards/<int:id>", methods=['GET'])
 @cross_origin()
@@ -77,16 +77,21 @@ def file_upload():
     user_id = 1  # Temporary user_id
     uploaded_files = request.files.getlist('file')
     for file in uploaded_files:
-        content = file.read().decode('utf-8')
-        upload_id = create_upload(user_id, file.filename)
+        file_name = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+        file.save(file_path)
+        
+        upload_id = create_upload(user_id, file_name)
         try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
             parse_hand_history(content, user_id, upload_id)
-            update_upload_status(upload_id, 'complete')
-        except Exception as e:  # Bad practice
+            update_upload_status(upload_id, 'completed')
+        except:  # Bad practice
             delete_upload(upload_id)
+        os.remove(file_path)
 
     return {'status': 'success', 'message': f'{len(uploaded_files)} files uploaded successfully!'}
-
 
 if __name__ == '__main__':
     cur.execute(open('./sql/R6/fetch_hand_query_templates.sql').read())
