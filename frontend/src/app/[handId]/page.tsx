161eb32e318a6774785f71d87f81adc16439985a
Matteo_psnt <matteo@szl.ai>
2024-07-09 19:32:04 -0400

    Fixed hand page

--- frontend/src/app/[handId]/page.tsx
+++ frontend/src/app/[handId]/page.tsx
@@ -1,57 +1,68 @@
 "use client";
-import React from "react";
+import { useState, useEffect } from "react";
 import MetaData from "./MetaData";
 import TableData from "./TableData";
 import { Table, TableHeader, TableHead, TableRow } from "@/components/ui/table";
 import { usePathname } from "next/navigation";
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export default async function HandDetails() { // Asynchronous server component for pulling api calls. // TODO: pass pathname somehow
 
-    var othiResult: Array<any> = [];
-    var paResult: Array<any> = [];
-    var pcResult: Array<any> = [];
 
+
+export default function HandDetails() { // Asynchronous server component for pulling api calls. // TODO: pass pathname somehow
     const pathname = usePathname();
 
-    const apiCall = async (searchTerm: string) => {
-        // Run SQL queries to fetch appropriate data. See server.py for further information.
-        await fetch(`${API_URL}hand_summary/${searchTerm}`)
-                .then(resp => resp.json())
-                .then(data => {
-                    othiResult = data;
-                });
+    const [othiResult, setResponse1]: [any, any] = useState([]);
+    const [paResult, setResponse2] : [any, any] = useState([]);
+    const [pcResult, setResponse3] : [any, any] = useState([]);
 
-        await fetch(`${API_URL}player_actions/${searchTerm}`)
-                .then(resp => resp.json())
-                .then(data => {
-                    paResult = data;
-                });
+    let rows: Array<any> = [];
 
-        await fetch(`${API_URL}player_cards/${searchTerm}`)
-                .then(resp => resp.json())
-                .then(data => {
-                    pcResult = data;
-                });
+    const handleSearch = async (searchTerm: string) => {
+        const othiResponse2 = await fetch(`${API_URL}hand_summary/${searchTerm}`);
+        const paResponse2 = await fetch(`${API_URL}player_actions/${searchTerm}`);
+        const pcResponse2 = await fetch(`${API_URL}player_cards/${searchTerm}`);
+
+        setResponse1(await othiResponse2.json());
+        setResponse2(await paResponse2.json());
+        setResponse3(await pcResponse2.json());
+
+
+
+        console.log(rows);
     }
 
-    await apiCall(pathname.slice(1)); 
+    useEffect(() => {
+        handleSearch(pathname.slice(1));
+    }, [])
 
-    var rows: Array<any> = [];
     var pc_index: number = 0;
-    for (var i = 0; i < paResult.length; i++) { // Create a variable number of rows equal to the number of actions.
-        for (var card_index = 0; card_index < pcResult.length; card_index++) { // Find tuples from player_cards related to a given player id
-            if (paResult[i].player_id === pcResult[card_index].player_id) {
-                pc_index = card_index;
-                break;
+    if (paResult && pcResult && pcResult[0]) {
+        for (var i = 0; i < paResult.length; i++) { // Create a variable number of rows equal to the number of actions.
+            for (var card_index = 0; card_index < pcResult.length; card_index++) { // Find tuples from player_cards related to a given player id
+            
+                if (paResult[i].player_id === pcResult[card_index].player_id) {
+                    pc_index = card_index;
+                    break;
+                }
             }
+
+            rows.push(<TableData betting_round={paResult[i].betting_round} betting_amount={paResult[i].amount} 
+                player_name={paResult[i].name} card_1={pcResult[pc_index].hole_card1} card_2={pcResult[pc_index].hole_card2}/>);
         }
-        rows.push(<TableData betting_round={paResult[i].betting_round} betting_amount={paResult[i].amount} 
-            player_name={paResult[i].name} card_1={pcResult[pc_index].hole_card1} card_2={pcResult[pc_index].hole_card2}/>);
     }
+
+    console.log(othiResult)
+    console.log(paResult);
+    console.log(pcResult);
+
+
     return (
+       
         <div className="bg-[#2C2C2C] text-white px-32"> {/* Global tailwind formatting for both child components.*/}
-            <MetaData handID={pathname.slice(1)} tableName={othiResult[0].table_name} timestamp={othiResult[0].played_at}/>
+            <MetaData handID={pathname.slice(1)} tableName={othiResult[0] && othiResult[0].table_name} timestamp={othiResult[0] && othiResult[0].played_at}/>
+
             <Table> 
                 <TableHeader className="text-[#31D2DD]">
                     <TableRow>
@@ -63,7 +74,10 @@ export default async function HandDetails() { // Asynchronous server component f
                     </TableRow>
                 </TableHeader>
                 {rows}
+
             </Table>
+
+            
         </div>
     );
 }
