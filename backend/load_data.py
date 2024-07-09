b7a82a1dd4b80075ff7f9a640f47958d398222e7
Matteo_psnt <matteo@szl.ai>
2024-07-09 17:36:31 -0400

    update test user to not break the user check

--- backend/load_data.py
+++ backend/load_data.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     hand_history_path = args.hand_history_path
 
-    create_user("test_user", "test_email", "test_password", "f273d736-807e-4f8e-b919-0bc7a558d59b")
+    create_user("test_user", "test@email.com", "test_password", "f273d736-807e-4f8e-b919-0bc7a558d59b")
     upload_id = create_upload(1, hand_history_path)
     with open(hand_history_path, 'r', encoding='utf-8') as file:
         content = file.read()
         parse_hand_history(content, 1, upload_id)
         update_upload_status(upload_id, 'completed')
-    create_user("user2", "testemail2", "test_password", "f273d736-807e-4f8e-b919-0bc7a558d59b")
+    create_user("user2", "test2@email.com", "test_password", "f273d736-807e-4f8e-b919-0bc7a558d59b")
