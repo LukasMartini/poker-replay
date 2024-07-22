77281beddc3d29575d7cbe03586afa138a010cc9
Matteo_psnt <matteo@szl.ai>
2024-07-22 16:19:33 -0400

    fix

--- backend/db_commands.py
+++ backend/db_commands.py
@@ -195,7 +195,7 @@ def player_actions_in_hand(user_id, hand_id):
     WHERE player_action.hand_id = %s AND (poker_session.user_id = %s OR authorized.user_id IS NOT NULL)
     """
     
-    return data
+    return execute_query(query, (user_id, hand_id, user_id), fetch=True, return_dict=True)
 
 def player_cards_in_hand(user_id, hand_id):
     query = """
