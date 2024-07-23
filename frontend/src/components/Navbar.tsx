e571aa72e6c486d7f0e718e2d2461ec5a9af0776
Eric Zhang <erizha.grad2022@my.stmc.bc.ca>
2024-07-23 15:13:03 -0400

    Update Navbar.tsx
    Logo

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,15 +3,44 @@ import Link from "next/link"
 import { Button } from './ui/button'
 import { useAuth } from '@/components/auth/AuthContext';
 
+const Logo = () => {
+    return (
+        <svg  width="200px" height="50px">
+        <g transform="translate(10, 10)">
+        <g transform="rotate(-10, 9, 15)">
+          <rect x="0" y="0" width="18" height="30" fill="white" strokeWidth="0.5" rx="2" stroke="black" />
+          
+              <text x="2" y="7" fontSize="6" fill="red">7</text>
+              <text x="5" y="20" fontSize="15" fill="red">♥</text>
+        
+        </g>
+        <g transform="translate(15, 0), rotate(10, 9, 15)">
+          <rect x="0" y="0" width="18" height="30" fill="white" strokeWidth="0.5" rx="2" stroke="black" />
+              <text x="2" y="7" fontSize="6" fill="black">2</text>
+              <text x="5" y="20" fontSize="15" fill="black">♣</text>
+        </g>
+        <text x="40" y="25" font-weight="bold" fontSize="20" fill="white">PokerReplay</text>
+
+      </g>
+      </svg>
+  
+    )
+}
+
 const Navbar = () => {
     const {auth, logout} = useAuth();
     
     return (
         <div className="w-full bg-[#2C2C2C] text-white py-4 px-8 shadow-md flex items-center justify-between">
             <Link href={'/'}>
-                <h2 className="text-2xl font-bold">
+                <>    
+                <Logo/>            
+                {/* <h2 className="float-left text-2xl font-bold">
+                   
                     PokerReplay
-                </h2>
+                </h2> */}
+
+                </>
             </Link>
 
             <div className="flex items-center space-x-8">
